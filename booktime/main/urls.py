263c95a51de7b3e2a1eef7764d619130f1fefe5c
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-03-10 04:53:50 +0800

    Add products page
    Register URL path to the products page. Create the template for products page. Render the products in the ProductListView.

--- booktime/main/urls.py
+++ booktime/main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about-us/', TemplateView.as_view(template_name='about_us.html'),
          name='about_us',),
     path('contact-us/', views.ContactUsView.as_view(), name='contact_us'),
+    path('products/<slug:tag>/', views.ProductListView.as_view(), name='products'),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
