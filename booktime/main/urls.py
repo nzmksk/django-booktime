66221836dbdea08223094d8cd0758aab07374013
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-03-14 07:09:53 +0800

    Add login page
    1. Create a custom ModelForm - AuthenticationForm for user login in forms.py.
    2. Register URL path to the login page.
    3. Create the template for login page.

--- booktime/main/urls.py
+++ booktime/main/urls.py
@@ -1,12 +1,15 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
-from main import models, views
+from main import forms, models, views
 
 urlpatterns = [
     path('about-us/', TemplateView.as_view(template_name='about_us.html'),
          name='about_us',),
     path('contact-us/', views.ContactUsView.as_view(), name='contact_us'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html',
+         form_class=forms.AuthenticationForm), name='login'),
     path('product/<slug:slug>/',
          DetailView.as_view(model=models.Product), name='product'),
     path('products/<slug:tag>/', views.ProductListView.as_view(), name='products'),
