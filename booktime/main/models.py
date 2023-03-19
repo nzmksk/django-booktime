9d340d6828b0cc27666ef7978ab382103cf02a0d
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-03-19 21:04:02 +0800

    Update models.py
    Add Address model to store user's address(es).

--- booktime/main/models.py
+++ booktime/main/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
 
 
 class UserManager(BaseUserManager):
@@ -87,3 +87,29 @@ class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product-images')
     thumbnail = models.ImageField(upload_to='product-thumbnails', null=True)
+
+
+class Address(models.Model):
+    SUPPORTED_COUNTRIES = (
+        ('uk', 'United Kingdom'),
+        ('us', 'United States of America'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    address1 = models.CharField('Address line 1', max_length=60)
+    address2 = models.CharField('Address line 2', max_length=60, blank=True)
+    zip_code = models.CharField('ZIP / Postal code', max_length=12)
+    city = models.CharField(max_length=60)
+    country = models.CharField(max_length=3, choices=SUPPORTED_COUNTRIES)
+
+    def __str__(self):
+        return ', '.join(
+            [
+                self.name,
+                self.address1,
+                self.address2,
+                self.zip_code,
+                self.city,
+                self.country,
+            ]
+        )
