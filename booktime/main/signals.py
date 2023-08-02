abbfdf52c022e74a803a2ac4c03ebff2407e1225
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-08-02 23:43:34 +0800

    Add signal - keep existing baskets upon login

--- booktime/main/signals.py
+++ booktime/main/signals.py
@@ -1,10 +1,11 @@
 from io import BytesIO
 import logging
 from PIL import Image
+from django.contrib.auth.signals import user_logged_in
 from django.core.files.base import ContentFile
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import ProductImage
+from .models import Basket, ProductImage
 
 THUMBNAIL_SIZE = (300, 300)
 
@@ -26,3 +27,23 @@ def generate_thumbnail(sender, instance, **kwargs):
     instance.thumbnail.save(instance.image.name, ContentFile(
         temp_thumbnail.read()), save=False)
     temp_thumbnail.close()
+
+
+@receiver(user_logged_in)
+def merge_baskets_if_found(sender, user, request, **kwargs):
+    anonymous_basket = getattr(request, "basket", None)
+    if anonymous_basket:
+        try:
+            loggedin_basket = Basket.objects.get(user=user, status=Basket.OPEN)
+            for line in anonymous_basket.basketline_set.all():
+                line.basket = loggedin_basket
+                line.save()
+            anonymous_basket.delete()
+            request.basket = loggedin_basket
+            logger.info("Merged basket to id %d", loggedin_basket.id)
+        except Basket.DoesNotExist:
+            anonymous_basket.user = user
+            anonymous_basket.save()
+            logger.info(
+                "Assigned user to basket id %d", anonymous_basket.id
+            )
