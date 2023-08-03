04d967c422b28bff5f7d14ad164e41b759bfc7ca
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2023-08-04 04:24:52 +0800

    Add model test case - test create order

--- booktime/main/tests/test_models.py
+++ booktime/main/tests/test_models.py
@@ -19,3 +19,55 @@ class TestModel(TestCase):
             active=False,
         )
         self.assertEqual(len(models.Product.objects.active()), 2)
+
+    def test_create_order_works(self):
+        p1 = models.Product.objects.create(
+            name="The cathedral and the bazaar",
+            price=Decimal("10.00"),
+        )
+        p2 = models.Product.objects.create(
+            name="Pride and Prejudice",
+            price=Decimal("2.00"),
+        )
+
+        user1 = models.User.objects.create_user("user1", "pw432joij")
+        billing = models.Address.objects.create(
+            user=user1,
+            name="John Doe",
+            address1="127 Strudel Road",
+            city="London",
+            country="uk",
+        )
+        shipping = models.Address.objects.create(
+            user=user1,
+            name="John Doe",
+            address1="123 Deacon Road",
+            city="London",
+            country="uk",
+        )
+
+        basket = models.Basket.objects.create(user=user1)
+        models.BasketLine.objects.create(basket=basket, product=p1)
+        models.BasketLine.objects.create(basket=basket, product=p2)
+
+        with self.assertLogs("main.models", level="INFO") as cm:
+            order = basket.create_order(billing, shipping)
+
+        self.assertGreaterEqual(len(cm.output), 1)
+
+        order.refresh_from_db()
+
+        self.assertEquals(order.user, user1)
+        self.assertEquals(order.billing_name, "John Doe")
+        self.assertEquals(order.billing_address1, "127 Strudel Road")
+        self.assertEquals(order.billing_city, "London")
+        self.assertEquals(order.billing_country, "uk")
+        self.assertEquals(order.shipping_name, "John Doe")
+        self.assertEquals(order.shipping_address1, "123 Deacon Road")
+        self.assertEquals(order.shipping_city, "London")
+        self.assertEquals(order.shipping_country, "uk")
+        
+        self.assertEquals(order.lines.all().count(), 2)
+        lines = order.lines.all()
+        self.assertEquals(lines[0].product, p1)
+        self.assertEquals(lines[1].product, p2)
